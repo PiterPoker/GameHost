6cc3b5bcd66ed1e4e6a608dd729d25c9b532f48c
Гребеньков Артем <a.hrebiankov@enova.by>
2024-11-12 10:01:44 +0300

    feat: Add Domain project

---
+++ GameDomain/Game.cs
@@ -0,0 +1,13 @@
+﻿namespace GameDomain
+{
+    public abstract class Game
+    {
+        public abstract void Start();
+        public abstract void Initialize(Setting settings);
+        public abstract Setting GetSettings();
+        public static Game Create<TGame>() where TGame : Game, new()
+        {
+            return new TGame();
+        }
+    }
+}
---
+++ GameDomain/Services/Configuration/JsonConfigurationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GameDomain.Services.Configuration
+{
+    public class JsonConfigurationService<TSettings> 
+        : IConfigurationService<TSettings> where TSettings : class
+    {
+        public TSettings GetSettings(string fullPath)
+        {
+            var jsonString = File.ReadAllText(fullPath);
+            return JsonSerializer.Deserialize<TSettings>(jsonString);
+        }
+    }
+}
